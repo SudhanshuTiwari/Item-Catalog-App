275b90bb7e78a138af513fe9c59ce76bcaf9fc07
Sudhanshu Tiwari <sid_1590@yahoo.in>
2018-09-19 01:21:26 +0530

    Update application.py
    updated port to default 5000 as per vagrantfile

--- application.py
+++ application.py
@@ -378,4 +378,4 @@ def showCategoryItemJSON(catalog_id, item_id):
 if __name__ == '__main__':
 	app.debug = True
 	app.secret_key = 'super_secret_key'
-	app.run(host = '0.0.0.0', port = 5001)
+	app.run(host = '0.0.0.0', port = 5000)
